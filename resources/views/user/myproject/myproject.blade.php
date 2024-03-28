a3c2cbfa78b2d251b90de6a68815906030d267de
Revangga <revanggaprakusya527@gmail.com>
2024-03-28 09:47:39 +0700

    fix:bug

--- resources/views/user/myproject/myproject.blade.php
+++ resources/views/user/myproject/myproject.blade.php
@@ -181,9 +181,7 @@
                     @endif
                 </div>
             </div>
-    @endforeach
-
-    <div id="popup-modal" tabindex="-1"
+            <div id="popup-modal" tabindex="-1"
         class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
         <div class="relative p-4 w-full max-w-md max-h-full">
             <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
@@ -221,6 +219,9 @@
             </div>
         </div>
     </div>
+    @endforeach
+
+    
 
     </div>
 @endsection
