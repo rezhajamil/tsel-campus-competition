9b7e8333aa4dbdc8da9a8be71b78182bfae6e46e
Revangga <revanggaprakusya527@gmail.com>
2024-07-19 13:05:09 +0700

    fix

--- resources/views/user/myproject/create-tim.blade.php
+++ resources/views/user/myproject/create-tim.blade.php
@@ -1,5 +1,13 @@
 <x-app-layout>
-    <div class="px-10 pt-4">
+    
+    <div class="px-10 pt-4 flex gap-4 items-center">
+        <a href="">
+            <button class="p-1 rounded-full border-2 border-red-500 text-red-500">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
+                  </svg>                  
+            </button>
+        </a>
         <h1 class="font-bold text-lg">{{ __('Add Team') }}</h1>
     </div>
     
